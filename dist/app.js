86ee35d34f54d4ce836a3a9a403d25b99a141a81
helciocandido <helcio.mcandido@gmail.com>
2022-07-14 17:13:37 -0400

    concluído desafio 2

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,25 @@
 "use strict";
 console.log("Hello world!");
+// Como podemos melhorar o esse código usando TS?
+var Trabalho;
+(function (Trabalho) {
+    Trabalho[Trabalho["Atriz"] = 0] = "Atriz";
+    Trabalho[Trabalho["Padeiro"] = 1] = "Padeiro";
+    Trabalho[Trabalho["Professor"] = 2] = "Professor";
+})(Trabalho || (Trabalho = {}));
 const pessoa1 = {};
-pessoa1.nome = "maria";
+pessoa1.nome = "Maria";
 pessoa1.idade = 29;
-pessoa1.profissao = "atriz";
+pessoa1.profissao = Trabalho.Atriz;
 let pessoa2 = {};
-pessoa2.nome = "roberto";
+pessoa2.nome = "Roberto";
 pessoa2.idade = 19;
-pessoa2.profissao = "Padeiro";
+pessoa2.profissao = Trabalho.Professor;
 let pessoa3 = {};
-pessoa3.nome = "laura";
+pessoa3.nome = "Laura";
 pessoa3.idade = 32;
-pessoa3.profissao = "Atriz";
+pessoa3.profissao = Trabalho.Atriz;
 let pessoa4 = {};
-pessoa4.nome = "carlos";
+pessoa4.nome = "Carlos";
 pessoa4.idade = 19;
-pessoa4.profissao = "padeiro";
+pessoa4.profissao = Trabalho.Padeiro;
--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,34 @@ console.log("Hello world!");
 
 // Como podemos melhorar o esse código usando TS?
 
+enum Trabalho {
+  "Atriz",
+  "Padeiro",
+  "Professor",
+}
+
 interface Pessoa {
   nome: string;
   idade: number;
-  profissao: string;
+  profissao: Trabalho;
 }
 
 const pessoa1 = {} as Pessoa;
-pessoa1.nome = "maria";
+pessoa1.nome = "Maria";
 pessoa1.idade = 29;
-pessoa1.profissao = "atriz";
+pessoa1.profissao = Trabalho.Atriz;
 
 let pessoa2 = {} as Pessoa;
-pessoa2.nome = "roberto";
+pessoa2.nome = "Roberto";
 pessoa2.idade = 19;
-pessoa2.profissao = "Padeiro";
+pessoa2.profissao = Trabalho.Professor;
 
 let pessoa3 = {} as Pessoa;
-pessoa3.nome = "laura";
+pessoa3.nome = "Laura";
 pessoa3.idade = 32;
-pessoa3.profissao = "Atriz";
+pessoa3.profissao = Trabalho.Atriz;
 
 let pessoa4 = {} as Pessoa;
-pessoa4.nome = "carlos";
+pessoa4.nome = "Carlos";
 pessoa4.idade = 19;
-pessoa4.profissao = "padeiro";
+pessoa4.profissao = Trabalho.Padeiro;
